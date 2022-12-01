e6e9bda6a7e097e2c8cff0c3c96a3c0fd32814a5
arun-nd <arun@navadhiti.com>
2022-12-01 20:23:59 +0530

    fix

--- app.js
+++ app.js
@@ -4,17 +4,18 @@ const path = require("path")
 const dotenv = require('dotenv')
 const jwt = require("jsonwebtoken")
 const bcrypt = require("bcryptjs")
+const request = require('request');
 
 dotenv.config({ path: './.env'})
 
 const app = express();
 
-const db = mysql.createConnection({
-    host: process.env.DATABASE_HOST,
-    user: process.env.DATABASE_USER,
-    password: process.env.DATABASE_PASSWORD,
-    database: process.env.DATABASE
-})
+// const db = mysql.createConnection({
+//     host: process.env.DATABASE_HOST,
+//     user: process.env.DATABASE_USER,
+//     password: process.env.DATABASE_PASSWORD,
+//     database: process.env.DATABASE
+// })
 
 const publicDir = path.join(__dirname, './public')
 
@@ -24,60 +25,39 @@ app.use(express.json())
 
 app.set('view engine', 'hbs')
 
-db.connect((error) => {
-    if(error) {
-        console.log(error)
-    } else {
-        console.log("MySQL connected!")
-    }
-})
+// db.connect((error) => {
+//     if(error) {
+//         console.log(error)
+//     } else {
+//         console.log("MySQL connected!")
+//     }
+// })
 
-app.get("/", (req, res) => {
+app.get("/home", (req, res) => {
     res.render("index")
 })
-
-app.get("/register", (req, res) => {
-    res.render("register")
-})
-
 app.get("/login", (req, res) => {
     res.render("login")
 })
 
-app.post("/auth/register", (req, res) => {    
-    const { name, email, password, password_confirm } = req.body
-
-    db.query('SELECT email FROM users WHERE email = ?', [email], async (error, result) => {
-        if(error){
-            console.log(error)
+app.get("/", (req, res) => {
+    var options = {
+        'method': 'POST',
+        'url': `https://insp.memberclicks.net/oauth/v1/token?grant_type=authorization_code&code=${req.query.code}&scope=read&redirect_uri=https://dev.isop.navadhiti.com`,
+        'headers': {
+          'Authorization': 'Basic NHlJSDBKVU10NFB6VkhqMWVNTGo6Y2JhZTcxOGI1YmYwNDdmN2EzODE2OTAxNzkzNGFlZDE=',
+          'Content-Type': 'application/x-www-form-urlencoded',
+          'Cache-Control': 'no-cache',
+          'Cookie': 'SESSION=MDFhZDBjYTAtZTc4Mi00NjdkLWEzY2EtZmFlZjg3MmZhZTUz; __cfruid=d9b3c5d9a5a7ae46e02a9c4a70ad8e09c343fb36-1669903335'
         }
-
-        if( result.length > 0 ) {
-            return res.render('register', {
-                message: 'This email is already in use'
-            })
-        } else if(password !== password_confirm) {
-            return res.render('register', {
-                message: 'This email is already in use'
-            })
-        }
-
-        let hashedPassword = await bcrypt.hash(password, 8)
-
-        console.log(hashedPassword)
-       
-        db.query('INSERT INTO users SET?', {name: name, email: email, password: hashedPassword}, (err, result) => {
-            if(error) {
-                console.log(error)
-            } else {
-                return res.render('register', {
-                    message: 'User registered!'
-                })
-            }
-        })        
-    })
+      };
+      request(options, function (error, response) {
+        if (error) throw new Error(error);
+        console.log(response.body);
+        res.send(response.body);
+      });
 })
 
-app.listen(5000, ()=> {
-    console.log("server started on port 5000")
+app.listen(8001, ()=> {
+    console.log("server started on port 8001")
 })
